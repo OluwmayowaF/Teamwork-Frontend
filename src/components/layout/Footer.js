ad41945f18fa5386e8460de7225a4c721a309179
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-20 23:15:03 +0100

    Feature(Add Header) Added the Header component and added to App.js[Starts 001]

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+
+function Footer() {
+    return (
+        <footer className="App-footer">
+            <p>Teamwork (C) 2019</p>
+            
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css';
+
+function Header() {
+    return (
+        
+        <header className="App-header">
+            <img src="https://res.cloudinary.com/oluwamayowaf/image/upload/v1574283398/TeamWorkFE/teamwork_fvc2kr.svg" alt="logo" />
+        </header>
+       
+       
+    )
+}
+
+export default Header;
