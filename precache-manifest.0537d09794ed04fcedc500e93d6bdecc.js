76d9dee07590471ae6e51db3c52b88b16d3a2c35
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-30 20:09:28 +0100

    Updates

---
+++ precache-manifest.0537d09794ed04fcedc500e93d6bdecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d075e825e001a9549d3b38adb4fd96c3",
+    "url": "/Teamwork-Frontend/index.html"
+  },
+  {
+    "revision": "577e9f54010b170c9680",
+    "url": "/Teamwork-Frontend/static/css/main.ec12d681.chunk.css"
+  },
+  {
+    "revision": "614c341fe4b0393a03b0",
+    "url": "/Teamwork-Frontend/static/js/2.bc12086b.chunk.js"
+  },
+  {
+    "revision": "577e9f54010b170c9680",
+    "url": "/Teamwork-Frontend/static/js/main.37ce53b4.chunk.js"
+  },
+  {
+    "revision": "19b8e851cdcaeb24deda",
+    "url": "/Teamwork-Frontend/static/js/runtime-main.84d3ac6a.js"
+  }
+]);
